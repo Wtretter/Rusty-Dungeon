a9f39af6e9f12d3582b96b2c0e210a769d2662e1
William Tretter <will@wtretter.com>
2026-01-11 17:59:19 -0600

    added bidirectional long-running connection

--- backend/main.go
+++ backend/main.go
@@ -8,12 +8,59 @@ import (
 
 func handler(connection net.Conn) {
 	defer connection.Close()
+	for {
+		message, err := recv_string(connection)
+		if err != nil {
+			println(err)
+			return
+		}
 
-	message := []byte("Hello, the connection is working")
+		println("Message Recv: ", message)
+
+		err = send_string(connection, message)
+		if err != nil {
+			println(err)
+			return
+		}
+	}
+}
+
+func recv_string(connection net.Conn) (string, error) {
+	buffer := make([]byte, 2)
+	var err error
+
+	_, err = connection.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	recv_len := binary.BigEndian.Uint16(buffer)
+	buffer = make([]byte, recv_len)
+
+	_, err = connection.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buffer), nil
+
+}
+
+func send_string(connection net.Conn, message string) error {
+	var err error
+	bin_message := []byte(message)
 	bin_len := make([]byte, 2)
-	binary.BigEndian.PutUint16(bin_len, uint16(len(message)))
-	connection.Write(bin_len)
-	connection.Write(message)
+
+	binary.BigEndian.PutUint16(bin_len, uint16(len(bin_message)))
+	_, err = connection.Write(bin_len)
+	if err != nil {
+		return err
+	}
+	_, err = connection.Write(bin_message)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
@@ -22,14 +69,14 @@ func main() {
 		log.Fatal(err)
 	}
 	defer listener.Close()
-
+	println("Server Up")
 	for {
 		// Wait for a connection.
 		connection, err := listener.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-
+		println("Connection established")
 		// Handle connection
 		go handler(connection)
 	}
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,25 +1,40 @@
 use std::str;
 use iced::widget::{button, column, text, Column};
 use iced::Task;
-use tokio::io::AsyncReadExt;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+use tokio::sync::Mutex;
+use std::sync::{Arc};
 
+type Connection = Arc<Mutex<TcpStream>>;
 const SERVER_ADDR: &str = "rustydungeon.wtretter.com:27010";
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum Message {
+    Startup,
     Increment,
     Decrement,
-    ServerConnected(Option<()>),
+    ServerConnected(Option<Connection>),
+    DataSent(Option<()>),
 }
 
 #[derive(Default)]
-struct Counter {
+struct Application {
     value: i64,
+    connection: Option<Connection>,
 }
 
-async fn connect_to_server() -> Option<()>{
-    let mut stream = TcpStream::connect(SERVER_ADDR).await.ok()?;
+async fn initialize() -> Message {
+    return Message::Startup;
+}
+
+async fn connect_to_server() -> Option<Connection>{
+    let conn = Arc::new(Mutex::new(TcpStream::connect(SERVER_ADDR).await.ok()?));
+    return Some(conn);
+}
+
+async fn recv_message(conn: Connection) -> Option<String>{
+    let mut stream = conn.lock().await;
 
     let mut size_buffer: [u8; 2] = [0, 0];
     stream.read_exact(&mut size_buffer).await.ok()?;
@@ -29,27 +44,70 @@ async fn connect_to_server() -> Option<()>{
     let mut string_buffer = vec![0; size];
     stream.read_exact(&mut string_buffer).await.ok()?;
 
-    let string = str::from_utf8(&string_buffer).ok()?;
-    println!("got message: {}", string);
+    let message = str::from_utf8(&string_buffer).ok()?;
+    return Some(message.to_string());
+}
+
+async fn send_message(conn: Connection, string: String) -> Option<()>{
+    {
+        let mut stream = conn.lock().await;
+
+        let len: u16 = string.len() as u16;
+        stream.write_u16(len).await.ok()?;
+
+        let mess_buffer = string.as_bytes();
+        stream.write_all(mess_buffer).await.ok()?;
+    }
+
+    let mess_recv = recv_message(conn.clone()).await?;
+    println!("{}", mess_recv);
 
     return Some(());
 }
 
-impl Counter {
+impl Application {
+    fn new() -> (Self, Task<Message>) {
+        (
+            Self {
+                value: 0,
+                connection: None,
+            },
+            Task::future(initialize())
+        )
+    }
+
     fn update(&mut self, message: Message) -> Task<Message> {
+        
         match message {
+            Message::Startup => {
+                println!("Starting up!");
+                return Task::perform(connect_to_server(), Message::ServerConnected);
+            }
             Message::Increment => {
                 self.value += 1;
-                return Task::perform(connect_to_server(), Message::ServerConnected);
+                match &mut self.connection {
+                    Some(conn) => {
+                        return Task::perform(send_message(conn.clone(), "asdf".to_string()), Message::DataSent);
+                    }
+                    None => {
+                        println!("No Connection")
+                    }
+                }
             }
             Message::Decrement => {
                 self.value -= 1;
             }
-            Message::ServerConnected(Some(_)) => {
+            Message::ServerConnected(Some(conn)) => {
+                self.connection = Some(conn);
                 println!("Connection succeeded!");
             }
             Message::ServerConnected(None) => {
                 println!("Connection failed!");
+            }
+            Message::DataSent(Some(_)) => {}
+            
+            Message::DataSent(None) => {
+                println!("data failed to send");
             }
         }
         return Task::none();
@@ -66,10 +124,10 @@ impl Counter {
         // The layout
         let interface = column![increment, counter, decrement];
 
-        interface
+        return interface;
     }
 }
 
 pub fn main() -> iced::Result {
-    iced::run(Counter::update, Counter::view)
+    iced::application(Application::new, Application::update, Application::view).run()
 }
